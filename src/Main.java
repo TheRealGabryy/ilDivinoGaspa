5ce9a972ea59b274ff8420081855c3dc4354d8b6
Gabriele B <the.real.gabryy@gmail.com>
2025-11-19 16:36:14 +0100

    Check nome

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,39 @@
+import java.util.Scanner;
+
 public class Main {
+    public static final Scanner scanner = new Scanner(System.in);
+
+
+
     public static final String[] mainMenu = {
             "Nuova Partita",
             "Carica partita (Coming Soon)",
             "Esci"
     };
 
-    public static void nuovaPartita() {
 
+    public static void nuovaPartita() {
+        System.out.println("Inserisci in nome del giocatore:");
+        String nome;
+        do {
+            nome = scanner.nextLine();
+            if (!checkNome(nome)) {
+                System.out.println("Inserisci un nome del giocatore corretto");
+            }
+        } while (!checkNome(nome));
     }
 
     public static void main(String[] args) {
         System.out.println("Benvenuto al gioco della divina commedia");
+    }
+
+    public static boolean checkNome(String nome) {
+        boolean hasSpecial = false;
+        for (int i = 0; i < nome.length(); i++) {
+            if (!Character.isLetter(nome.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 }
