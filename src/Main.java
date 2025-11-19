e388638a4a5ca2e9b73291c2d2f2db58612317dc
Gabriele B <the.real.gabryy@gmail.com>
2025-11-19 16:31:39 +0100

    Nuova Partita e menu

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,14 @@ public class Main {
     public static final String[] mainMenu = {
             "Nuova Partita",
             "Carica partita (Coming Soon)",
-            "Tutorial",
             "Esci"
     };
 
+    public static void nuovaPartita() {
+
+    }
 
     public static void main(String[] args) {
         System.out.println("Benvenuto al gioco della divina commedia");
-
     }
 }
---
+++ src/System/Menu.java
@@ -0,0 +1,17 @@
+package System;
+
+public class Menu {
+    public static void printMenu(String title, String[] options) {
+        String line = "========= Menu =========";
+        System.out.println(line);
+        System.out.println(line);
+        System.out.println(line);
+        for (int i = 1; i <= options.length; i++) {
+
+        }
+
+
+
+
+    }
+}
