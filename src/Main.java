daf38fe2265d37cfebfeb0d33708abf795117ca6
Gabriele B <the.real.gabryy@gmail.com>
2025-11-19 16:26:15 +0100

    Main Menu Add

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
+    public static final String[] mainMenu = {
+            "Nuova Partita",
+            "Carica partita (Coming Soon)",
+            "Tutorial",
+            "Esci"
+    };
+
+
     public static void main(String[] args) {
+        System.out.println("Benvenuto al gioco della divina commedia");
 
     }
 }
