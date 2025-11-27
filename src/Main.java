79aa353809772a9e8ed16d341601f37d6a1e0f82
Gabriele B <the.real.gabryy@gmail.com>
2025-11-27 19:21:00 +0100

    Aggiunte feature per la creazione della partita, menus, e tutorial base

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import System.*;
+
 import java.util.Scanner;
 
 public class Main {
     public static final Scanner scanner = new Scanner(System.in);
-
+    public static String nome;
 
 
     public static final String[] mainMenu = {
@@ -10,25 +12,73 @@ public class Main {
             "Carica partita (Coming Soon)",
             "Esci"
     };
-
-
-    public static void nuovaPartita() {
-        System.out.println("Inserisci in nome del giocatore:");
-        String nome;
-        do {
-            nome = scanner.nextLine();
-            if (!checkNome(nome)) {
-                System.out.println("Inserisci un nome del giocatore corretto");
-            }
-        } while (!checkNome(nome));
+    public static void printBasicTutorial() throws InterruptedException/*questo è semplicemente l'errore che da quando il THREAD fallisce
+    (mai essendo che è un banalissimo .sleep), lo da intelliJ in automatico*/
+    { //questo è il tutorial BASE BASE del gioco, come comandi, e come funziona in termini di tasti.
+        System.out.println("Ti trovi all'inferno");
+        Thread.sleep(1500);
+        System.out.println("Non hai idea di come ci sei finito qui...");
+        Thread.sleep(1500);
+        System.out.println("Il tuo obiettivo è quello di uscire.");
+        Thread.sleep(1500);
+        System.out.println("Potrai parlare con dei personaggi");
+        System.out.println("che ti daranno oggetti necessari per avanzare");
+        Thread.sleep(1500);
+        System.out.println("Troverai sfide, ostacoli, e punti ciechi");
+        Thread.sleep(1500);
+        System.out.println("Ma puoi sempre fare affidamento sulla tua Guida.");
+        System.out.println("Nuon Divertimento!");
     }
 
-    public static void main(String[] args) {
+    public static void nuovaPartita() throws InterruptedException {
+        scanner.nextLine();
+        System.out.println("Inserisci in nome del giocatore");
+        do {
+            System.out.print("-> ");
+            nome = scanner.nextLine();
+            if (!checkNome(nome)) {
+                System.out.println("Inserisci un nome per il giocatore corretto!");
+            }
+        } while (!checkNome(nome));
+        printBasicTutorial();
+        System.out.println("---- Premi invio per iniziare! ");
+        String invio = scanner.nextLine();
+
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Benvenuto al gioco della divina commedia");
+        boolean continua = true;
+        int choice = 0;
+        do {
+            Menu.printMenu("Initial Menu", mainMenu);
+            try {
+                choice = scanner.nextInt();
+            } catch (Exception e) {
+                System.out.println("Non hai inserito un intero");
+            }
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Nouva partita"); //Inizio la partita, metodi SOPRA
+                    nuovaPartita();
+                    break;
+                case 2:
+                    System.out.println("Questa feature è coming soon"); //alla fine non fa nulla
+                    break;
+                case 3:
+                    System.out.println("Uscita programma"); //chiunde il programma
+                    continua = false;
+                    break;
+                default: //SE l'utente inserisce qualsiasi numero diverso da quelli possibili, andrà qui, questo crea un semplicissimo controllo.
+                    System.out.println("Non hai inserito un'opzione valida");
+                    break;
+            }
+        } while (continua);
     }
 
     public static boolean checkNome(String nome) {
-        boolean hasSpecial = false;
         for (int i = 0; i < nome.length(); i++) {
             if (!Character.isLetter(nome.charAt(i))) {
                 return false;
--- src/System/Menu.java
+++ src/System/Menu.java
@@ -4,14 +4,12 @@ public class Menu {
     public static void printMenu(String title, String[] options) {
         String line = "========= Menu =========";
         System.out.println(line);
+        System.out.println(title);
         System.out.println(line);
-        System.out.println(line);
-        for (int i = 1; i <= options.length; i++) {
-
+        for (int i = 0; i < options.length; i++) {
+            System.out.println((i+1) + " = " + options[i]);
         }
-
-
-
-
+        System.out.println(line);
+        System.out.print("La tua Opzione: ");
     }
 }
